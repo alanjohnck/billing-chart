28eac8f26ea6963d87893eb78cd7c5b351ca31d9
alanjohnck <118127993+alanjohnck@users.noreply.github.com>
2023-05-08 22:47:37 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,1 @@
-import React from 'react'
 
-
-function Navbar() {
-  return (
-    <>
-    <div className='navbar'>
-    <h1>BILLING CHART</h1>
-    </div>
-    </>
-  )
-}
-
-export default Navbar
